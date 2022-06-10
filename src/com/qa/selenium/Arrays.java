221a7ae406629f6ae75b275cd6eb711f7355cfd1
SRK\shri <shrikumarhavaragi126@gmail.com>
2022-06-10 22:04:05 +0530

    script added

---
+++ src/com/qa/selenium/Arrays.java
@@ -0,0 +1,14 @@
+package com.qa.selenium;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String[] arr1= {"selenium","utf","Jemeter"};
+        for(String i:arr1)
+        {
+        	System.out.println(i);
+        }
+	}
+
+}
---
+++ src/com/qa/selenium/Methods.java
@@ -0,0 +1,21 @@
+package com.qa.selenium;
+
+public class Methods {
+	
+	public int add() {
+		int a=5;
+		int b=5;
+		int sum=a+b;
+		System.out.println(sum);
+		return sum;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Methods obj1=new Methods();
+		obj1.add();
+
+	}
+
+}
